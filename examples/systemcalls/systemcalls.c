3b308e301fe417bec62929c08503e997384883df
micjun <jung.michael@protonmail.com>
2024-02-06 20:22:14 -0500

    assignment-3-part-1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,9 @@
 #include "systemcalls.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,6 +21,12 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int system_status;
+    system_status = system(cmd);
+    if (system_status == -1)
+    {
+        return false;
+    }
 
     return true;
 }
@@ -58,6 +69,29 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    pid_t process_id;
+    int system_status;
+    fflush(stdout);
+    process_id = fork();
+
+    if (process_id == -1)
+    {
+        return false;
+    }
+    else if (process_id == 0 )
+    {
+        execv(command[0], command);
+        exit(1);
+    }
+    else
+    {
+        waitpid(process_id, &system_status, 0);
+        if(WIFEXITED(system_status))
+        {
+            return WEXITSTATUS(system_status) == 0;
+        }
+        return false;
+    }
 
     va_end(args);
 
@@ -92,7 +126,36 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    pid_t process_id;
+    int system_status;
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    fflush(stdout);
+    process_id = fork();
 
+    switch (process_id)
+    {
+        case -1: 
+            perror("FORK");
+            return false;
+        case 0: 
+            if (dup2(fd, 1) < 0)
+		    {
+			    perror("DUP2");
+			    return false;
+		    }
+		    close(fd);
+		    execv(command[0], command);
+		    exit(1);
+        default:
+            close(fd);
+            waitpid(process_id, &system_status, 0);
+            if (WIFEXITED(system_status)) 
+            {
+                return WEXITSTATUS(system_status) == 0;
+            }
+    }
+    
+    
     va_end(args);
 
     return true;
